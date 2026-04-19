7e0b72bb6cbc1eae4ffb57dc967916af4d8c61b0
jugl3r <amirhakimov55@gmail.com>
2026-04-19 14:44:34 +0300

    edit lab4

--- Lab-4/obj/Debug/net10.0/Lab-4.AssemblyInfo.cs
+++ Lab-4/obj/Debug/net10.0/Lab-4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab-4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8438ff25418ba1f204efa773c259774da1b56fcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3f5c419dc875c0d13c26cf281366e351e2404b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab-4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab-4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
